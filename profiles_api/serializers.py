f5206c05a0036167ad3ac66df6f5e3398b2c4eb5
EugenDaniel <=>
2021-02-01 00:03:02 +0100

    Added login API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -31,12 +31,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         return user
 
-    def update(self, instance, validate_data):
+    def update(self, instance, validated_data):
         """Handle updating user account"""
-        if 'password' in validate_data:
-            password = validate_data.pop('Password')
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
             instance.set_password(password)
 
-        return super().update(instance, validate_data)
+        return super().update(instance, validated_data)
 
         return user
